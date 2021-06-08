02a6e2b49d763fa444b621c39a00e5cde8b77c70
victor <victorfreitas1494@gmail.com>
2021-06-07 23:46:03 -0300

    remocao de comentario

--- php/ConectDB.php
+++ php/ConectDB.php
@@ -5,12 +5,4 @@
     $basedados = "tbunp";
 
     $connect = new mysqli($Server, $User, $Passe, $basedados);
-
-    /*
-    $resultado = $connect->query("SELECT nome FROM tb_usuarios");
-
-    $row = $resultado->fetch_assoc();
-
-    echo htmlentities($row['nome']);
-    */
 ?>
