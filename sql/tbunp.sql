6204ab957ef4a1abd908c230f89418f54c0e2c50
victor <victorfreitas1494@gmail.com>
2021-06-14 02:24:59 -0300

    mudanca de comandos sql

--- sql/tbunp.sql
+++ sql/tbunp.sql
@@ -5,7 +5,7 @@ CREATE TABLE tb_usuario(
     nome varchar(120),
     data_nasc date,
     login varchar(20),
-    sernha varchar(50),
+    senha varchar(50),
     cargo varchar(20)
 );
 
@@ -34,6 +34,16 @@ CREATE TABLE permissao(
     permissoes varchar(50)
 );
 
+INSERT INTO permissao (permissoes) VALUES ('administrador');
+INSERT INTO permissao (permissoes) VALUES ('juiz');
+INSERT INTO permissao (permissoes) VALUES ('usuario');
+
 ALTER TABLE tb_eventos ADD CONSTRAINT pkUser_fkEventos FOREIGN KEY (fk_usuario) REFERENCES tb_usuario (id_usuario) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 ALTER TABLE tb_medalhas ADD  CONSTRAINT pkEventos_fkMedalhas FOREIGN KEY (fk_eventos) REFERENCES tb_eventos (id_eventos) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+
+CREATE TABLE `tbunp`.`tb_usuario` ( `id_usuario` INT NOT NULL AUTO_INCREMENT , `nome` VARCHAR(120) NOT NULL , `data_nasc` DATE NOT NULL , `login` VARCHAR(20) NOT NULL , `senha` VARCHAR(50) NOT NULL , `cargo` VARCHAR(20) NOT NULL , PRIMARY KEY (`id_usuario`)) ENGINE = InnoDB;
+
+CREATE TABLE `tbunp`.`tb_evento` ( `id_evento` INT NOT NULL AUTO_INCREMENT , `nome_evento` VARCHAR(120) NOT NULL , `data_evt` DATE NOT NULL , `fk_usuario` INT NOT NULL , PRIMARY KEY (`id_evento`)) ENGINE = InnoDB;
