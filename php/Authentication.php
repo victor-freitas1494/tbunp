5b7aed0c59b9f3c43b07e7ab39caa4073da49bbf
victor <victorfreitas1494@gmail.com>
2021-06-14 02:23:49 -0300

    iniciar session

--- php/Authentication.php
+++ php/Authentication.php
@@ -1,11 +1,11 @@
 <?php
     include_once("ConectDB.php");
+    session_start();
 
-    $resultado = $connect->query("SELECT login, senha FROM tb_usuarios WHERE login = '{$_POST['login']}' AND senha = MD5('{$_POST['password']}')");
-
+    $resultado = $connect->query("SELECT * FROM tb_usuario WHERE login = '{$_POST['login']}' AND senha = MD5('{$_POST['password']}')");
     $row = $resultado->fetch_assoc();
-
     if($row['login'] === $_POST['login'] && $row['senha'] === md5($_POST['password'])){
+        $_SESSION['usuario'] = $row;
         echo true;
     }else{
         echo false;
