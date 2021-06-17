4df66166ef95070d83e58b435d8ec716f76aec4f
Nicholas <nicksextrememafra@gmail.com>
2021-06-16 22:02:34 -0300

    Acresentado js botao cancelar

--- javascript/redirencionar.js
+++ javascript/redirencionar.js
@@ -3,3 +3,9 @@ var link = document.getElementById('btn-novo');
 link.addEventListener('click',function(){
     window.location.href = "../sistema/newuser.php";
 });
+
+var cancelar = document.getElementById('btn_cancelar');
+
+cancelar.addEventListener('click',function(){
+    window.location.href = "../sistema/dashboardUser.php";
+});
