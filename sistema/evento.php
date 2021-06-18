5d6588f00a5c8b518944bf3b676e8a532f3ea813
Nicholas <nicksextrememafra@gmail.com>
2021-06-18 19:40:21 -0300

    Alterações em front end

--- sistema/evento.php
+++ sistema/evento.php
@@ -16,6 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/master.css">
     <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboardUser.css">
     <link rel="stylesheet" href="../css/evento.css">
     
     <title>Evento</title>
@@ -81,7 +82,7 @@
       </div>
       <?php if($_SESSION['usuario']['cargo'] == 'administrador' || $_SESSION['usuario']['cargo'] == 'juiz'){?>
       <div class="d-grid gap-2 mt-2 margem">
-        <a href="newEvent.php" role="button" class="btn btn-primo margi" style="color: black;" id="btn_novoEvento">
+        <a href="newEvent.php" role="button" class="btn btn-pai margi" style="color: black;" id="btn_novoEvento">
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
