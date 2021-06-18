901915d0f6197ae6b671b4cfba2910e736d2dcb6
Nicholas <nicksextrememafra@gmail.com>
2021-06-17 21:15:17 -0300

    Implementação de novo botao de cadastro

--- sistema/dashboardUser.php
+++ sistema/dashboardUser.php
@@ -85,7 +85,12 @@
     </table>
     </div>
     <div class="sysUser margemNovo">
-      <input type="button" value="Novo Cadastro" class="btn btn-primary" id="btn-novo">
+            <a role="button" class="btn btn-primo margi" style="color: black;" id="btn-novo">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+              </svg>
+            </a>
     </div>
   </div>
 
@@ -113,6 +118,8 @@
           <?php 
             if ($total > 0){
               do {
+                if($linha['cargo'] != 'administrador'){
+
           ?>
           <tr>
 
@@ -122,14 +129,19 @@
             <td><a href="dashboardmodific.php?user=<?=$linha['id_usuario']?>">Editar</a></td>
           </tr>
 
-          <?php } while ($linha = mysqli_fetch_assoc($exibirmed));
+          <?php }} while ($linha = mysqli_fetch_assoc($exibirmed));
             }
           ?>
         </tbody>
       </table>
     </div>
     <div class="sysUser margemNovo">
-            <input type="button" value="Novo Cadastro" class="btn btn-primary" id="btn-novo">
+            <a role="button" class="btn btn-primo margi" style="color: black;" id="btn-novo">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+              </svg>
+            </a>
     </div>
   </div>
 
